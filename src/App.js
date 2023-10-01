2d67c0d6b0f716256956e5e2033d5c42217f2369
nftteaze <t72915318@gmail.com>
2023-10-01 11:23:59 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ChromeDinoGame from 'react-chrome-dino';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <h1>Dash Runner</h1>
+    <ChromeDinoGame />
+  </div>
+);
 
 export default App;
