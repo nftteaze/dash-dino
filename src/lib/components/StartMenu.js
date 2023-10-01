7d64ad6586fa4a4d0ad7603ce6447ba7b8b09a12
nftteaze <t72915318@gmail.com>
2023-10-01 19:41:12 +0100

    Start Menu

--- src/lib/components/StartMenu.js
+++ src/lib/components/StartMenu.js
@@ -1,11 +1,18 @@
 // StartMenu.js
 import React from 'react';
+import './Dino.css'; // Import the Dino.css file
 
-const StartMenu = ({ onStartClick }) => (
-  <div className="start-menu">
-    <h1>Welcome to Dash Runner</h1>
-    <button onClick={onStartClick}>Start Game</button>
-  </div>
-);
+const StartMenu = ({ onStartClick }) => {
+  return (
+    <div className="start-menu">
+      <h1>Welcome to</h1>
+      <h1>
+        <span className="dash">Dash</span>
+        <span className="runner">Runner</span>
+      </h1>
+      <button onClick={onStartClick}>Start Game</button>
+    </div>
+  );
+};
 
 export default StartMenu;
