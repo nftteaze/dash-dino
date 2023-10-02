fba1d1e5795d7569f7826354deb0c574b64283d4
nftteaze <t72915318@gmail.com>
2023-10-02 05:57:52 +0100

    night

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-// App.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ChromeDinoGame from 'react-chrome-dino';
 import './lib/components/Dino.css';
 import StartMenu from './lib/components/StartMenu';
 
 const App = () => {
+  const [isNightMode, setIsNightMode] = useState(false);
   const [gameStarted, setGameStarted] = useState(false);
 
   const handleStartClick = () => {
     setGameStarted(true);
   };
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      // Prevent spacebar from toggling night mode when the game is active
+      if (event.keyCode === 32 && gameStarted) {
+        event.preventDefault();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [gameStarted]);
+
+  useEffect(() => {
+    if (isNightMode) {
+      document.body.classList.add('night-mode');
+    } else {
+      document.body.classList.remove('night-mode');
+    }
+  }, [isNightMode]);
 
   return (
     <div className="app-container">
@@ -17,9 +40,9 @@ const App = () => {
         <StartMenu onStartClick={handleStartClick} />
       ) : (
         <>
-          <h1 className="title">
-            <span className="dash black">Dash</span>
-            <span className="dash">Runner</span>
+          <h1 className={`title ${isNightMode ? 'night-mode' : ''}`}>
+            <span className="dash">Dash</span>
+            <span className="runner">Runner</span>
           </h1>
           <ChromeDinoGame />
         </>
--- src/lib/components/StartMenu.js
+++ src/lib/components/StartMenu.js
@@ -1,15 +1,12 @@
 // StartMenu.js
+
 import React from 'react';
-import './Dino.css'; // Import the Dino.css file
+import './StartMenu.css';
 
 const StartMenu = ({ onStartClick }) => {
   return (
     <div className="start-menu">
-      <h1>Welcome to</h1>
-      <h1>
-        <span className="dash">Dash</span>
-        <span className="runner">Runner</span>
-      </h1>
+      <h1><span className="dash">Dash</span><span className="runner">Runner</span></h1>
       <button onClick={onStartClick}>Start Game</button>
     </div>
   );
